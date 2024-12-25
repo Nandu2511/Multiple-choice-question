3358c0e32040567ef013a3d0c3f54ff7a9da3017
Nandeeshwar <nandeeshwar@gmail.com>
2024-12-25 13:45:52 +0530

    commit mcq

--- src/main/java/com/mcq/McqApplication.java
+++ src/main/java/com/mcq/McqApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class McqApplication {
 
-	
 	public static void main(String[] args) {
 		SpringApplication.run(McqApplication.class, args);
 	}
