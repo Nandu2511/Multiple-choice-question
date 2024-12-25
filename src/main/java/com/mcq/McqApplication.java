b322d9b5e2e467c8a5a58b39b94bd3bfac6a16c6
krishnagopal <krishnagopal@mygetepay.com>
2024-12-25 13:39:19 +0530

    commit multipul choice question main class

--- src/main/java/com/mcq/McqApplication.java
+++ src/main/java/com/mcq/McqApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class McqApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(McqApplication.class, args);
 	}
